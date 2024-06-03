9ffbaacefdc48d1eaef74c0da33306bcc6605b41
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-06-03 18:24:23 +0300

    Test refactorisation

---
+++ src/controller/shared/Index.java
@@ -0,0 +1,24 @@
+package controller.shared;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Index extends HttpServlet {
+    private void processRequest(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+        response.sendRedirect("index.jsp");
+    }
+
+    // Override methods
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+}
