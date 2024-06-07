7204f0cab5a6160e7b2666e4625fe9cb8de8c324
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-06-07 18:23:44 +0300

    Creation classe Utilisateur

---
+++ src/model/client/Utilisateur.java
@@ -0,0 +1,79 @@
+package model.client;
+
+import java.sql.Date;
+
+public class Utilisateur{
+    private int id; 
+    private String nom;
+    private String prenom;
+    private String email;
+    private Date date_de_naissance;
+    private int id_genre;
+
+    /* Constructor */
+    public Utilisateur(int id, String nom, String prenom, String mail, Date dtn, int id_g){
+        this.set_id(id);
+        this.set_nom(nom);
+        this.set_prenom(prenom);
+        this.set_email(mail);
+        this.set_date_de_naissance(dtn);
+        this.set_id_genre(id_g);
+    }
+
+    public Utilisateur(String nom, String prenom, String mail, Date dtn, int id_g){
+        this.set_nom(nom);
+        this.set_prenom(prenom);
+        this.set_email(mail);
+        this.set_date_de_naissance(dtn);
+        this.set_id_genre(id_g);
+    }
+
+    /* Setters */
+    public void set_id(int n_id){
+        this.id = n_id;
+    }
+    public void set_nom(String str){
+        this.nom = str;
+    }
+    public void set_prenom(String str){
+        this.prenom = str;
+    }
+    public void set_email(String mail){
+        this.email = mail;
+    }
+    public void set_date_de_naissance(Date dt){
+        this.date_de_naissance = dt;
+    }
+    public void set_id_genre(int id){
+        this.id_genre = id;
+    }
+
+    /* Getters */
+    public int get_id(){
+        return this.id;
+    }
+    public String get_nom(){
+        return this.nom;
+    }
+    public String get_prenom(){
+        return this.prenom;
+    }
+    public String get_email(){
+        return this.email;
+    }
+    public Date get_date_de_naissance(){
+        return this.date_de_naissance;
+    }
+    public int get_id_genre(){
+        return this.id_genre;
+    }
+
+    public static void main(String[] argv ){
+        Utilisateur u = new Utilisateur("Sanda","Silakiniaiana","sanda@gmail.com",null,1);
+        if(u != null){
+            System.out.println("Not null");
+        }else{
+            System.out.println("Utilisateur null");
+        }
+    }
+}
---
+++ src/script/client/views.sql
@@ -0,0 +1,9 @@
+SELECT 
+    id_utilisateur,
+    nom_utilisateur,
+    prenom_utilisateur,
+    email,
+    date_naissance,
+    id_genre
+    FROM utilisateur
+;
