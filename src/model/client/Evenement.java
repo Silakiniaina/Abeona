a9cbd3f6e21d1e43ab18615646a7b4fb633d38bb
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-06-08 11:32:38 +0300

    Creation fonction pour avoir la liste des evenements

--- src/model/client/Evenement.java
+++ src/model/client/Evenement.java
@@ -1,6 +1,12 @@
 package model.client;
 
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import model.shared.Database;
 
 public class Evenement{
     private int id_evenement;
@@ -10,6 +16,9 @@ public class Evenement{
     private String ville; 
     private Hotel hotel;
 
+    static int EVENEMENT_PASSE = 0;
+    static int EVENEMENT_FUTUR = 1;
+
     /* Constructor */
     public Evenement(String nom, Date dt, String desc,  String ville, Hotel h){
         this.set_nom_evenement(nom);
@@ -17,6 +26,43 @@ public class Evenement{
         this.set_description(desc);
         this.set_ville(ville);
         this.set_hotel(h);
+    }
+
+    /* Fonction pour avoir la liste des evenement du calendrier */
+    public static ArrayList<Evenement> get_liste_evenement_calendrier(int type)throws Exception{
+        ArrayList<Evenement> resultat = new ArrayList<Evenement>();
+        Connection c = null;
+        PreparedStatement prsmt  = null;
+        ResultSet rs = null; 
+
+        try{
+            c = Database.get_connection();
+            if(c != null){
+                if(type == 0){
+                    prsmt = c.prepareStatement("SELECT * FROM v_evenement_calendrier_passe");
+                }else if( type == 1){
+                    prsmt = c.prepareStatement("SELECT * FROM v_evenement_calendrier_futur");
+                }else{
+                    prsmt = c.prepareStatement("SELECT * FROM v_evenement_calendrier");
+                }
+                rs = prsmt.executeQuery();
+                while (rs.next()) {
+                    Evenement ev = new Evenement(rs.getString(2), rs.getDate(3), rs.getString(4), rs.getString(5),null);
+                    ev.set_id_evenement(rs.getInt(1));
+                    resultat.add(ev);
+                }
+            }else{
+                throw new Exception("Aucune connexion ");
+            }
+        }catch(Exception e){
+            throw e;
+        }finally{
+            if(rs != null){ rs.close(); }
+            if(prsmt != null){ prsmt.close(); }
+            if(c != null){ c.close(); }
+        }
+
+        return resultat;
     }
     
     /* Getters */
@@ -57,6 +103,18 @@ public class Evenement{
     }
     public void set_hotel(Hotel hotel) {
         this.hotel = hotel;
+    }
+
+    /* Test */
+    public static void main(String[] args) {
+        try{
+            ArrayList<Evenement> ls = Evenement.get_liste_evenement_calendrier(Evenement.EVENEMENT_FUTUR);
+            for(Evenement ev : ls){
+                System.out.println("nom : "+ev.get_nom_evenement()+" , date : "+ev.get_date_evenement());
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
     
 }
