b0bc83e7ec800e03f09ec7fcf935929ac874b62d
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-06-11 20:47:45 +0300

    creation fonction rechercher_attraction

--- src/model/client/Attraction.java
+++ src/model/client/Attraction.java
@@ -1,5 +1,11 @@
 package model.client;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import model.shared.Database;
 public class Attraction {
     private String id_attraction;
     private String nom_attraction;
@@ -11,8 +17,33 @@ public class Attraction {
     }
     
     /* Fonction pour rechercher des attraction */
-    public static ArrayList<Attraction> rechercher_attraction(String dest){
-        
+    public static ArrayList<Attraction> rechercher_attraction(String dest)throws Exception{
+        ArrayList<Attraction> resultat = new ArrayList<Attraction>();
+        Connection c = null;
+        PreparedStatement prsmt  = null;
+        ResultSet rs = null; 
+
+        try{
+            c = Database.get_connection();
+            if(c != null){
+                prsmt = c.prepareStatement("SELECT * FROM attraction WHERE nom_attraction LIKE '%?%'");
+                prsmt.setString(1, dest);
+                rs = prsmt.executeQuery();
+                while (rs.next()) {
+                    Attraction a = new Attraction(rs.getString(1), rs.getString(2));
+                    resultat.add(a);
+                }
+            }else{
+                throw new Exception("Aucune connexion ");
+            }
+        }catch(Exception e){
+            throw e;
+        }finally{
+            if(rs != null){ rs.close(); }
+            if(prsmt != null){ prsmt.close(); }
+            if(c != null){ c.close(); }
+        }
+        return resultat;
     }
     /* Getters */
     public String get_id_attraction() {
