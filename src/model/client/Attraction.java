4bca17a4ac5613ad7a5c9d3a5c0c8e4e6e199a52
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-06-11 20:32:40 +0300

    Creation classe Attraction(Simulation)

---
+++ src/model/client/Attraction.java
@@ -0,0 +1,28 @@
+package model.client;
+
+public class Attraction {
+    private String id_attraction;
+    private String nom_attraction;
+
+    /* Constructor */
+    public Attraction(String id, String name){
+        this.set_id_attraction(id);
+        this.set_nom_attraction(name);
+    }
+    
+    /* Getters */
+    public String get_id_attraction() {
+        return id_attraction;
+    }
+    public String get_nom_attraction() {
+        return nom_attraction;
+    }
+
+    /* Setters */
+    public void set_id_attraction(String id_attraction) {
+        this.id_attraction = id_attraction;
+    }
+    public void set_nom_attraction(String nom_attraction) {
+        this.nom_attraction = nom_attraction;
+    }
+}
