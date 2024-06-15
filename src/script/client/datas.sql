d4112ecfea9e1fb5c59e32fc016c3ff069414379
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-06-15 20:33:03 +0300

    Renouvelement script sql

--- src/script/client/datas.sql
+++ src/script/client/datas.sql
@@ -1,131 +1,271 @@
-INSERT INTO categorie_utilisateur(libelle) VALUES
-    ('client'),
-    ('admin')
+-- Insertion des données pour la table Genre
+INSERT INTO Genre (libelle) VALUES
+('Masculin'),
+('Féminin'),
+('Autre')
 ;
 
-INSERT INTO genre(libelle) VALUES 
-    ('Homme'),
-    ('Femme'),
-    ('N/A')
+-- Insertion des données pour la table Categorie_utilisateur
+INSERT INTO Categorie_utilisateur (libelle) VALUES
+('Utilisateur standard'),
+('Utilisateur premium'),
+('Administrateur'),
+('Modérateur'),
+('Support technique');
+
+-- Insertion des données pour la table Categorie_attraction
+INSERT INTO Categorie_attraction (libelle) VALUES
+('Parc National'),
+('Musée'),
+('Plage'),
+('Monument historique'),
+('Zoo');
+
+-- Insertion des données pour la table Province
+INSERT INTO Province (nom_province, description) VALUES
+('Antananarivo', 'Capitale politique de Madagascar'),
+('Toamasina', 'Port principal de Madagascar'),
+('Antsiranana', 'Ville portuaire au nord de Madagascar'),
+('Fianarantsoa', 'Centre administratif au centre de Madagascar'),
+('Toliara', 'Ville côtière au sud-ouest de Madagascar');
+
+-- Insertion des données pour la table Point_interet
+INSERT INTO Point_interet (libelle) VALUES
+('Avenue de Indépendance'),
+('Parc Zoologique de Tsimbazaza'),
+('Baie de Diego-Suarez'),
+('Avenue Ranavalona III'),
+('Parc National de Isalo');
+
+-- Insertion des données pour la table categorie_evenement
+INSERT INTO categorie_evenement (libelle) VALUES
+('Conférence'),
+('Festival'),
+('Concert'),
+('Exposition'),
+('Compétition sportive');
+
+-- Insertion des données pour la table categorie_hotel
+INSERT INTO categorie_hotel (libelle) VALUES
+('Hôtel de luxe'),
+('Hôtel économique'),
+('Auberge de jeunesse'),
+('Villa'),
+('Résidence hôtelière');
+
+-- Insertion des données pour la table partenaire
+INSERT INTO partenaire (nom_partenaire, email_partenaire, mot_de_passe) VALUES
+('Hotel Madagascar', 'hotel.madagascar@example.com', '12345'),
+('Transport Express', 'transport.express@example.com', '12345'),
+('Guide Touristique', 'guide.touristique@example.com', '12345'),
+('Attraction Tropicale', 'attraction.tropicale@example.com', '12345'),
+('Agence de Voyage Aventure', 'agence.aventure@example.com', '12345');
+
+-- Insertion des données pour la table commodite
+INSERT INTO commodite (libelle) VALUES
+('Wi-Fi gratuit'),
+('Piscine'),
+('Spa'),
+('Restaurant'),
+('Salle de sport');
+
+-- Insertion des données pour la table categorie_chambre
+INSERT INTO categorie_chambre (libelle) VALUES
+('Chambre Standard'),
+('Chambre Deluxe'),
+('Suite Junior'),
+('Suite Présidentielle'),
+('Appartement');
+
+-- Insertion des données pour la table categorie_transport
+INSERT INTO categorie_transport (libelle) VALUES
+('Voiture de Location'),
+('Bus Touristique'),
+('Train Express'),
+('Vol Local'),
+('Croisière Fluviale');
+
+-- Insertion des données pour la table region
+INSERT INTO region (nom_region, id_province) VALUES
+('Analamanga', 'PRO1'),
+('Atsinanana', 'PRO2'),
+('Diana', 'PRO3'),
+('Haute Matsiatra', 'PRO4'),
+('Atsimo-Andrefana', 'PRO5');
+
+-- Insertion des données pour la table Guide
+INSERT INTO Guide (nom_guide, description, disponibilite, id_partenaire) VALUES
+('Jean Guide', 'Guide expérimenté à Madagascar', 1, 'PAR3'),
+('Emma Guide', 'Spécialiste des parcs nationaux', 1, 'PAR3'),
+('Luc Guide', 'Guide touristique à Antananarivo', 0, 'PAR3'),
+('Sophie Guide', 'Guide pour les circuits culturels', 1, 'PAR3'),
+('Pierre Guide', 'Guide pour les excursions en montagne', 1, 'PAR3');
+
+-- Insertion des données pour la table categorie_pack
+INSERT INTO categorie_pack ( libelle) VALUES
+('Aventure'),
+('Détente'),
+('Familial'),
+('Romantique'),
+('Économique');
+
+-- Insertion des données pour la table language_
+INSERT INTO language_ ( libelle) VALUES
+('Français'),
+('Anglais'),
+('Malagasy'),
+('Espagnol'),
+('Allemand');
+
+-- Insertion des données pour la table categorie_avis
+INSERT INTO categorie_avis ( libelle) VALUES
+('Hôtel'),
+('Pack'),
+('Transport'),
+('Attraction'),
+('Événement');
+
+-- Insertion des données pour la table categorie_evaluation
+INSERT INTO categorie_evaluation ( libelle) VALUES
+('Hôtel'),
+('Pack'),
+('Transport'),
+('Attraction'),
+('Événement');
+
+-- Insertion des données pour la table categorie_reservation
+INSERT INTO categorie_reservation (libelle) VALUES
+('Hôtel'),
+('Pack'),
+('Transport'),
+('Attraction'),
+('Événement');
+
+-- Insertion des données pour la table Utilisateur
+INSERT INTO Utilisateur (nom_utilisateur, prenom_utilisateur, date_de_naissance, email, mot_de_passe, id_genre, id_categorie_utilisateur) VALUES
+('Dupont', 'Jean', '1990-05-15', 'jean.dupont@example.com','12345', 'GEN1', 'CTU1'),
+('Durand', 'Sophie', '1985-08-20', 'sophie.durand@example.com','12345', 'GEN2', 'CTU2'),
+('Martin', 'Pierre', '1982-10-10', 'pierre.martin@example.com','12345', 'GEN1', 'CTU3'),
+('Lefevre', 'Emma', '1995-03-25', 'emma.lefevre@example.com','12345', 'GEN2', 'CTU4'),
+('Leroy', 'Luc', '1998-07-30', 'luc.leroy@example.com','12345', 'GEN1', 'CTU5');
+
+-- Insertion des données pour la table Ville
+INSERT INTO Ville (nom_ville, id_region) VALUES
+('Antananarivo', 'REG1'),
+('Toamasina', 'REG2'),
+('Antsiranana', 'REG3'),
+('Fianarantsoa', 'REG4'),
+('Toliara', 'REG5');
+
+--Insertion des donnees pour la table attraction
+INSERT INTO attraction(nom_attraction,description,id_categorie_attraction,id_ville) VALUES 
+('Test 1','lahflfdhfdhfal','CAT1','VIL1'),
+('Test 2','lahflfdhfdhfal','CAT1','VIL2'),
+('Test 3','lahflfdhfdhfal','CAT2','VIL3'),
+('Test 4','lahflfdhfdhfal','CAT4','VIL3'),
+('Test 5','lahflfdhfdhfal','CAT3','VIL3'),
+('Test 6','lahflfdhfdhfal','CAT5','VIL3'),
+('Test 7','lahflfdhfdhfal','CAT5','VIL4'),
+('Test 8','lahflfdhfdhfal','CAT3','VIL5')
 ;
 
-INSERT INTO utilisateur(nom_utilisateur,prenom_utilisateur,email,date_naissance,id_genre,id_categorie_utilisateur,mot_de_passe) VALUES
-    ('Ravelonarivo','Silakiniaina Sanda','sandasilakiniaina4@gmail.com','2005-07-12','GEN1','CTU2',digest('admin','sha1'))
-;
+-- Insertion des données pour la table Hotel
+INSERT INTO Hotel ( nom_hotel, description, adresse_hotel, id_partenaire, id_categorie_hotel, id_ville) VALUES
+('Hôtel Luxe Ivato', 'Hôtel de luxe près de aéroport', 'Ivato', 'PAR1', 'CHO1', 'VIL1'),
+('Auberge de la Côte Est', 'Auberge économique avec vue sur la mer', 'Route du Littoral', 'PAR2', 'CHO2', 'VIL2'),
+('Villa Sérénité', 'Villa avec piscine privée', 'Baie de Diego-Suarez', 'PAR3', 'CHO3', 'VIL3'),
+('Résidence Hôtelière Betsileo', 'Résidence hôtelière au centre-ville', 'Fianarantsoa Centre', 'PAR1', 'CHO4', 'VIL4'),
+('Hôtel Plage Tsimelahy', 'Hôtel près de la plage de Tsimelahy', 'Plage Tsimelahy', 'PAR2', 'CHO5', 'VIL5');
 
-INSERT INTO categorie_attraction(libelle) VALUES    
-    ('Plage'),
-    ('Desert'),
-    ('Foret')
-;
+-- Insertion des données pour la table chambre
+INSERT INTO chambre ( capacite, tarif, id_categorie_chambre, id_hotel) VALUES
+(2, 200.00, 'CCH1', 'HOT1'),
+(1, 150.00, 'CCH2', 'HOT2'),
+(4, 300.00, 'CCH3', 'HOT3'),
+(2, 250.00, 'CCH4', 'HOT4'),
+(2, 180.00, 'CCH5', 'HOT5');
 
-INSERT INTO preference_utilisateur(id_utilisateur,id_categorie_attraction) VALUES
-    ('CLT1','CTA1'),
-    ('CLT1','CTA2')
-;
+-- Insertion des données pour la table transport
+INSERT INTO transport (nom_transport, description,tarif, id_partenaire, id_categorie_transport) VALUES
+('Voiture de Location Aéroport', 'Service de location de voiture à aéroport', 120000.00,'PAR1', 'CTR1'),
+('Bus Touristique Madagascar', 'Circuit touristique en bus à Madagascar', 89000.00,'PAR2', 'CTR2'),
+('Train Express Central', 'Train express entre les grandes villes',1200000.00 ,'PAR2','CTR3'),
+('Vol Local Madagascar', 'Vol intérieur entre les villes de Madagascar',300000.00 ,'PAR2','CTR4'),
+('Croisière Fluviale Pangalanes', 'Croisière fluviale sur le canal des Pangalanes', 430000.00, 'PAR2', 'CTR5');
 
-INSERT INTO point_interet (libelle) VALUES
-    ('Métro moderne'),
-    ('Ragoût de mouton'),
-    ('Montagne majestueuse'),
-    ('Festival de musique'),
-    ('Musée Art Moderne'),
-    ('Réseau de bus efficace'),
-    ('Fruits de mer frais'),
-    ('Vieille ville'),
-    ('Parc national'),
-    ('Marché local')
-;
+-- Insertion des données pour la table Avis
+INSERT INTO Avis (avis_utilisateur, id_partenaire, id_utilisateur, id_categorie_avis) VALUES
+('Très bon service!', 'PAR1', 'USR1', 'CAV1'),
+('À améliorer la propreté.', 'PAR2', 'USR2', 'CAV2'),
+('Expérience neutre.', 'PAR3', 'USR3', 'CAV3'),
+('Réclamation sur la réservation.', 'PAR4', 'USR4', 'CAV4'),
+('Suggestion pour plus de commodités.', 'PAR5', 'USR5', 'CAV5');
 
-INSERT INTO province (nom_province, description_province) VALUES
-    ('Antananarivo', 'Capitale de Madagascar, connue pour ses collines et ses marchés animés.'),
-    ('Fianarantsoa', 'Région viticole avec des paysages vallonnés et des plantations de thé.'),
-    ('Toamasina', 'Important port maritime avec des plages et une vie nocturne animée.'),
-    ('Mahajanga', 'Ville côtière réputée pour ses plages et son ambiance détendue.'),
-    ('Antsiranana', 'Située a l extrême nord, connue pour ses baies pittoresques et ses parcs nationaux.'),
-    ('Toliara', 'Région du sud-ouest célèbre pour ses récifs coralliens et ses forêts de baobabs.')
-;
+-- Insertion des données pour la table Evaluation
+INSERT INTO Evaluation (id_utilisateur,evaluation, id_partenaire, id_categorie_evaluation) VALUES
+('USR1',4.5, 'PAR1', 'CEV1'),
+('USR2',3.8, 'PAR2', 'CEV2'),
+('USR3',3.0, 'PAR3', 'CEV3'),
+('USR4',2.5, 'PAR4', 'CEV4'),
+('USR1',4.4, 'PAR5', 'CEV5');
 
--- Antananarivo
-INSERT INTO point_interet_province (id_province, id_point_interet) VALUES
-    ('PRO1', 'POI1'),  
-    ('PRO1', 'POI2'),  
-    ('PRO1', 'POI3'), 
-    ('PRO2', 'POI4'),  
-    ('PRO2', 'POI5'),  
-    ('PRO2', 'POI6'), 
-    ('PRO3', 'POI7'),  
-    ('PRO3', 'POI8'),  
-    ('PRO3', 'POI9'),  
-    ('PRO4', 'POI10'), 
-    ('PRO4', 'POI1'),  
-    ('PRO4', 'POI2'),  
-    ('PRO5', 'POI3'), 
-    ('PRO5', 'POI4'),  
-    ('PRO5', 'POI5'),
-    ('PRO6', 'POI6'),
-    ('PRO6', 'POI7'),  
-    ('PRO6', 'POI8')
-;
+INSERT INTO pack(nom_pack,id_categorie_pack,id_transport,id_attraction,id_hotel) VALUES 
+('Pack Explorer','CPK1','TRN1','ATT2','HOT1'),
+('Pack Master','CPK2','TRN2','ATT1','HOT2'),
+('Pack Ultra','CPK3','TRN3','ATT3','HOT3');
 
-INSERT INTO ville (nom_ville, id_province) VALUES
-    ('Antananarivo', 'PRO1'),  
-    ('Antsirabe', 'PRO1'),     
-    ('Ambatolampy', 'PRO1'),   
-    ('Fianarantsoa', 'PRO2'),   
-    ('Ambalavao', 'PRO2'),      
-    ('Manakara', 'PRO2'),       
-    ('Toamasina', 'PRO3'),      
-    ('Brickaville', 'PRO3'),    
-    ('Foulpointe', 'PRO3'),     
-    ('Mahajanga', 'PRO4'),      
-    ('Marovoay', 'PRO4'),       
-    ('Ambato-Boeny', 'PRO4'),   
-    ('Antsiranana', 'PRO5'),    
-    ('Ambanja', 'PRO5'),        
-    ('Nosy Be', 'PRO5'),        
-    ('Toliara', 'PRO6'),        
-    ('Morondava', 'PRO6'),      
-    ('Tulear', 'PRO6')
-;  
+-- Insertion des données pour la table Achat_pack
+INSERT INTO achat_pack (prix, date_achat, id_utilisateur, id_pack) VALUES
+(500.00, '2023-01-15', 'USR1', 'PAK1'),
+(700.00, '2023-02-28', 'USR2', 'PAK2'),
+(900.00, '2023-03-10', 'USR3', 'PAK3'),
+(1200.00, '2023-04-20', 'USR4', 'PAK2'),
+(800.00, '2023-05-05', 'USR5', 'PAK1');
 
-INSERT INTO hotel (nom_hotel) VALUES
-    ('Hotel Colbert'),
-    ('Carlton Hotel'),
-    ('Hotel Tamboho'),
-    ('Hotel Thermes'),
-    ('Hotel Vanilla')
-;
+-- Insertion des données pour la table Reservation
+INSERT INTO Reservation (date_debut_reservation, date_fin_reservation, nombre_personne, prix, id_utilisateur, id_categorie_reservation) VALUES
+('2023-06-01', '2023-06-07', 2, 700.00, 'USR1', 'CRS1'),
+('2023-07-10', '2023-07-15', 1, 500.00, 'USR1', 'CRS2'),
+('2023-08-20', '2023-08-25', 4, 1200.00, 'USR2', 'CRS3'),
+('2023-09-05', '2023-09-10', 3, 900.00, 'USR1', 'CRS4'),
+('2023-10-15', '2023-10-20', 2, 800.00, 'USR2', 'CRS5');
 
-INSERT INTO evenement (nom_evenement, date_evenement, description_evenement, id_ville, id_hotel) VALUES
-    ('Festival de Jazz', '2024-07-10', 'Un festival de jazz annuel avec des musiciens internationaux.', 'VIL1', 'HOT1'),
-    ('Conférence sur le climat', '2024-08-15', 'Conférence internationale sur les changements climatiques.', 'VIL2', 'HOT2'),
-    ('Salon de l automobile', '2024-09-20', 'Présentation des derniers modèles de voitures.', 'VIL3', NULL),
-    ('Marathon de Tana', '2024-10-05', 'Un marathon annuel dans la capitale.', 'VIL1', 'HOT3'),
-    ('Festival des arts', '2024-11-12', 'Exposition d arts locaux et internationaux.', 'VIL4', NULL),
-    ('Fête nationale', '2024-06-26', 'Célébration de la fête nationale avec des parades et des feux d artifice.', 'VIL5', 'HOT3'),
-    ('Conférence médicale', '2024-07-22', 'Conférence sur les nouvelles avancées médicales.', 'VIL3', NULL),
-    ('Festival de la mer', '2024-08-30', 'Célébration des ressources maritimes avec des activités de plage.', 'VIL7', 'HOT5'),
-    ('Salon du livre', '2024-09-15', 'Rencontre avec des auteurs et présentation de nouveaux livres.', 'VIL8', NULL),
-    ('Festival du film', '2024-10-25', 'HOTjection de films internationaux et nationaux.', 'VIL9', 'HOT1'),
-    ('Exposition de photographie', '2024-11-18', 'Exposition de photographies de divers artistes.', 'VIL10', 'HOT2'),
-    ('Conférence technologique', '2024-12-05', 'Présentation des dernières innovations technologiques.', 'VIL11', NULL),
-    ('Tournoi de tennis', '2024-06-10', 'Tournoi de tennis avec des joueurs locaux et internationaux.', 'VIL12', 'HOT3'),
-    ('Festival de musique', '2024-07-18', 'Festival de musique avec des artistes de divers genres.', 'VIL13', NULL),
-    ('Exposition de voitures anciennes', '2024-08-22', 'Exposition de voitures classiques et anciennes.', 'VIL14', 'HOT4')
-;
+-- Insertion des données pour la table Evenement
+INSERT INTO Evenement (description, lieu_evenement, titre_evenement, id_hotel, id_ville, id_categorie_evenement) VALUES
+('Conférence sur le développement durable', 'Salle de Conférence A', 'Conférence Éco', 'HOT1', 'VIL1', 'CEV1'),
+('Concert de Jazz en plein air', 'Place des Arts', 'Jazz sous les Étoiles', 'HOT1', 'VIL2', 'CEV2'),
+('Festival de été', 'Parc Festival', 'Festival Été 2023', 'HOT3', 'VIL3', 'CEV3'),
+('Match de Hockey', 'Centre Sportif A', 'Hockey sur Glace', 'HOT4', 'VIL4', 'CEV4'),
+('Exposition  Art Moderne', 'Musée des Beaux-Arts', 'Expo Art Moderne','HOT5', 'VIL5', 'CEV5');
 
-INSERT INTO attraction (nom_attraction) VALUES
-    ('Tour Eiffel'),
-    ('Musée du Louvre'),
-    ('Mont Saint-Michel'),
-    ('Cathédrale Notre-Dame de Paris'),
-    ('Château de Versailles'),
-    ('Pont du Gard'),
-    ('Côte  Azur'),
-    ('Château de Chambord'),
-    ('Dune du Pilat'),
-    ('Mont Blanc')
-;
+-- Insertion des données pour la table Preference_utilisateur
+INSERT INTO Preference_utilisateur (id_utilisateur, id_categorie_attraction) VALUES
+('USR1', 'CAT1'),
+('USR2', 'CAT2'),
+('USR3', 'CAT3'),
+('USR4', 'CAT4'),
+('USR5', 'CAT5');
 
+-- Insertion des données pour la table Commodite_hotel
+INSERT INTO Commodite_hotel (id_hotel, id_commodite) VALUES
+('HOT1', 'COM1'),
+('HOT2', 'COM2'),
+('HOT3', 'COM3'),
+('HOT4', 'COM4'),
+('HOT5', 'COM5');
 
+-- Insertion des données pour la table Point_interet_ville
+INSERT INTO Point_interet_ville (id_ville, id_point_interet) VALUES
+('VIL1', 'PIN1'),
+('VIL2', 'PIN2'),
+('VIL3', 'PIN3'),
+('VIL4', 'PIN4'),
+('VIL5', 'PIN5');
+
+-- Insertion des données pour la table Language_guide
+INSERT INTO Language_guide (id_guide, id_language) VALUES
+('GUI1', 'LAN1'),
+('GUI2', 'LAN2'),
+('GUI3', 'LAN3'),
+('GUI4', 'LAN4'),
+('GUI5', 'LAN5');
--- src/script/client/functions.sql
+++ src/script/client/functions.sql
@@ -1,16 +1,3 @@
-CREATE OR REPLACE FUNCTION generer_id_client()
-RETURNS TEXT AS $$
-DECLARE
-    client_seqval TEXT;
-    client_id TEXT;
-BEGIN
-    SELECT nextval('client_seq') INTO client_seqval;
-    client_id := 'CLT' || client_seqval;
-    RETURN client_id;
-END;
-$$
-LANGUAGE plpgsql;
-
 CREATE OR REPLACE FUNCTION generer_id_genre()
 RETURNS TEXT AS $$
 DECLARE
@@ -44,34 +31,8 @@ DECLARE
     categorie_attraction_id TEXT;
 BEGIN
     SELECT nextval('categorie_attraction_seq') INTO categorie_attraction_seqval;
-    categorie_attraction_id := 'CTA' || categorie_attraction_seqval;
+    categorie_attraction_id := 'CAT' || categorie_attraction_seqval;
     RETURN categorie_attraction_id;
-END;
-$$
-LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION generer_id_preference_utilisateur()
-RETURNS TEXT AS $$
-DECLARE
-    preference_utilisateur_seqval TEXT;
-    preference_utilisateur_id TEXT;
-BEGIN
-    SELECT nextval('preference_utilisateur_seq') INTO preference_utilisateur_seqval;
-    preference_utilisateur_id := 'PRU' || preference_utilisateur_seqval;
-    RETURN preference_utilisateur_id;
-END;
-$$
-LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION generer_id_point_interet()
-RETURNS TEXT AS $$
-DECLARE
-    point_interet_seqval TEXT;
-    point_interet_id TEXT;
-BEGIN
-    SELECT nextval('point_interet_seq') INTO point_interet_seqval;
-    point_interet_id := 'POI' || point_interet_seqval;
-    RETURN point_interet_id;
 END;
 $$
 LANGUAGE plpgsql;
@@ -89,15 +50,197 @@ END;
 $$
 LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION generer_id_point_interet_province()
+CREATE OR REPLACE FUNCTION generer_id_point_interet()
 RETURNS TEXT AS $$
 DECLARE
     point_interet_seqval TEXT;
     point_interet_id TEXT;
 BEGIN
-    SELECT nextval('point_interet_province_seq') INTO point_interet_seqval;
-    point_interet_id := 'PIP' || point_interet_seqval;
+    SELECT nextval('point_interet_seq') INTO point_interet_seqval;
+    point_interet_id := 'PIN' || point_interet_seqval;
     RETURN point_interet_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION generer_id_categorie_evenement()
+RETURNS TEXT AS $$
+DECLARE
+    categorie_evenement_seqval TEXT;
+    categorie_evenement_id TEXT;
+BEGIN
+    SELECT nextval('categorie_evenement_seq') INTO categorie_evenement_seqval;
+    categorie_evenement_id := 'CEV' || categorie_evenement_seqval;
+    RETURN categorie_evenement_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION generer_id_categorie_hotel()
+RETURNS TEXT AS $$
+DECLARE
+    categorie_hotel_seqval TEXT;
+    categorie_hotel_id TEXT;
+BEGIN
+    SELECT nextval('categorie_hotel_seq') INTO categorie_hotel_seqval;
+    categorie_hotel_id := 'CHO' || categorie_hotel_seqval;
+    RETURN categorie_hotel_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION generer_id_partenaire()
+RETURNS TEXT AS $$
+DECLARE
+    partenaire_seqval TEXT;
+    partenaire_id TEXT;
+BEGIN
+    SELECT nextval('partenaire_seq') INTO partenaire_seqval;
+    partenaire_id := 'PAR' || partenaire_seqval;
+    RETURN partenaire_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION generer_id_commodite()
+RETURNS TEXT AS $$
+DECLARE
+    commodite_seqval TEXT;
+    commodite_id TEXT;
+BEGIN
+    SELECT nextval('commodite_seq') INTO commodite_seqval;
+    commodite_id := 'COM' || commodite_seqval;
+    RETURN commodite_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION generer_id_categorie_chambre()
+RETURNS TEXT AS $$
+DECLARE
+    categorie_chambre_seqval TEXT;
+    categorie_chambre_id TEXT;
+BEGIN
+    SELECT nextval('categorie_chambre_seq') INTO categorie_chambre_seqval;
+    categorie_chambre_id := 'CCH' || categorie_chambre_seqval;
+    RETURN categorie_chambre_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION generer_id_categorie_transport()
+RETURNS TEXT AS $$
+DECLARE
+    categorie_transport_seqval TEXT;
+    categorie_transport_id TEXT;
+BEGIN
+    SELECT nextval('categorie_transport_seq') INTO categorie_transport_seqval;
+    categorie_transport_id := 'CTR' || categorie_transport_seqval;
+    RETURN categorie_transport_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION generer_id_region()
+RETURNS TEXT AS $$
+DECLARE
+    region_seqval TEXT;
+    region_id TEXT;
+BEGIN
+    SELECT nextval('region_seq') INTO region_seqval;
+    region_id := 'REG' || region_seqval;
+    RETURN region_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION generer_id_guide()
+RETURNS TEXT AS $$
+DECLARE
+    guide_seqval TEXT;
+    guide_id TEXT;
+BEGIN
+    SELECT nextval('guide_seq') INTO guide_seqval;
+    guide_id := 'GUI' || guide_seqval;
+    RETURN guide_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION generer_id_categorie_pack()
+RETURNS TEXT AS $$
+DECLARE
+    categorie_pack_seqval TEXT;
+    categorie_pack_id TEXT;
+BEGIN
+    SELECT nextval('categorie_pack_seq') INTO categorie_pack_seqval;
+    categorie_pack_id := 'CPK' || categorie_pack_seqval;
+    RETURN categorie_pack_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION generer_id_language()
+RETURNS TEXT AS $$
+DECLARE
+    language_seqval TEXT;
+    language_id TEXT;
+BEGIN
+    SELECT nextval('language_seq') INTO language_seqval;
+    language_id := 'LAN' || language_seqval;
+    RETURN language_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION generer_id_categorie_avis()
+RETURNS TEXT AS $$
+DECLARE
+    categorie_avis_seqval TEXT;
+    categorie_avis_id TEXT;
+BEGIN
+    SELECT nextval('categorie_avis_seq') INTO categorie_avis_seqval;
+    categorie_avis_id := 'CAV' || categorie_avis_seqval;
+    RETURN categorie_avis_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION generer_id_categorie_evaluation()
+RETURNS TEXT AS $$
+DECLARE
+    categorie_evaluation_seqval TEXT;
+    categorie_evaluation_id TEXT;
+BEGIN
+    SELECT nextval('categorie_evaluation_seq') INTO categorie_evaluation_seqval;
+    categorie_evaluation_id := 'CEV' || categorie_evaluation_seqval;
+    RETURN categorie_evaluation_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION generer_id_categorie_reservation()
+RETURNS TEXT AS $$
+DECLARE
+    categorie_reservation_seqval TEXT;
+    categorie_reservation_id TEXT;
+BEGIN
+    SELECT nextval('categorie_reservation_seq') INTO categorie_reservation_seqval;
+    categorie_reservation_id := 'CRS' || categorie_reservation_seqval;
+    RETURN categorie_reservation_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION generer_id_utilisateur()
+RETURNS TEXT AS $$
+DECLARE
+    utilisateur_seqval TEXT;
+    utilisateur_id TEXT;
+BEGIN
+    SELECT nextval('utilisateur_seq') INTO utilisateur_seqval;
+    utilisateur_id := 'USR' || utilisateur_seqval;
+    RETURN utilisateur_id;
 END;
 $$
 LANGUAGE plpgsql;
@@ -128,28 +271,28 @@ END;
 $$
 LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION generer_id_evenement()
+CREATE OR REPLACE FUNCTION generer_id_chambre()
 RETURNS TEXT AS $$
 DECLARE
-    evenement_seqval TEXT;
-    evenement_id TEXT;
+    chambre_seqval TEXT;
+    chambre_id TEXT;
 BEGIN
-    SELECT nextval('evenement_seq') INTO evenement_seqval;
-    evenement_id := 'EVE' || evenement_seqval;
-    RETURN evenement_id;
+    SELECT nextval('chambre_seq') INTO chambre_seqval;
+    chambre_id := 'CHM' || chambre_seqval;
+    RETURN chambre_id;
 END;
 $$
 LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION generer_id_categorie_evenement()
+CREATE OR REPLACE FUNCTION generer_id_transport()
 RETURNS TEXT AS $$
 DECLARE
-    categorie_evenement_seqval TEXT;
-    categorie_evenement_id TEXT;
+    transport_seqval TEXT;
+    transport_id TEXT;
 BEGIN
-    SELECT nextval('categorie_evenement_seq') INTO categorie_evenement_seqval;
-    categorie_evenement_id := 'CEV' || categorie_evenement_seqval;
-    RETURN categorie_evenement_id;
+    SELECT nextval('transport_seq') INTO transport_seqval;
+    transport_id := 'TRN' || transport_seqval;
+    RETURN transport_id;
 END;
 $$
 LANGUAGE plpgsql;
@@ -160,9 +303,87 @@ DECLARE
     attraction_seqval TEXT;
     attraction_id TEXT;
 BEGIN
-    SELECT nextval('attraction_seq') INTO  attraction_seqval;
-    attraction_id := 'ATR' ||  attraction_seqval;
+    SELECT nextval('attraction_seq') INTO attraction_seqval;
+    attraction_id := 'ATT' || attraction_seqval;
     RETURN attraction_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION generer_id_pack()
+RETURNS TEXT AS $$
+DECLARE
+    pack_seqval TEXT;
+    pack_id TEXT;
+BEGIN
+    SELECT nextval('pack_seq') INTO pack_seqval;
+    pack_id := 'PAK' || pack_seqval;
+    RETURN pack_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION generer_id_avis()
+RETURNS TEXT AS $$
+DECLARE
+    avis_seqval TEXT;
+    avis_id TEXT;
+BEGIN
+    SELECT nextval('avis_seq') INTO avis_seqval;
+    avis_id := 'AVI' || avis_seqval;
+    RETURN avis_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION generer_id_evaluation()
+RETURNS TEXT AS $$
+DECLARE
+    evaluation_seqval TEXT;
+    evaluation_id TEXT;
+BEGIN
+    SELECT nextval('evaluation_seq') INTO evaluation_seqval;
+    evaluation_id := 'EVA' || evaluation_seqval;
+    RETURN evaluation_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION generer_id_achat_pack()
+RETURNS TEXT AS $$
+DECLARE
+    achat_pack_seqval TEXT;
+    achat_pack_id TEXT;
+BEGIN
+    SELECT nextval('achat_pack_seq') INTO achat_pack_seqval;
+    achat_pack_id := 'ACP' || achat_pack_seqval;
+    RETURN achat_pack_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION generer_id_reservation()
+RETURNS TEXT AS $$
+DECLARE
+    reservation_seqval TEXT;
+    reservation_id TEXT;
+BEGIN
+    SELECT nextval('reservation_seq') INTO reservation_seqval;
+    reservation_id := 'RES' || reservation_seqval;
+    RETURN reservation_id;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION generer_id_evenement()
+RETURNS TEXT AS $$
+DECLARE
+    evenement_seqval TEXT;
+    evenement_id TEXT;
+BEGIN
+    SELECT nextval('evenement_seq') INTO evenement_seqval;
+    evenement_id := 'EVT' || evenement_seqval;
+    RETURN evenement_id;
 END;
 $$
 LANGUAGE plpgsql;
