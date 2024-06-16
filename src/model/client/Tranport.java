d9dc254abec259a1125e210fcdb5a766cb450904
Silakiniaina <sandasilakiniaina4@gmail.com>
2024-06-16 23:41:35 +0300

    Surdefintion des fonctions abstraites

--- src/model/client/Tranport.java
+++ src/model/client/Tranport.java
@@ -1,5 +1,11 @@
 package model.client;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import model.shared.Database;
+
 public class Tranport extends Partenaire{
     private double tarif;
     private String id_categorie_transport;
@@ -32,26 +38,42 @@ public class Tranport extends Partenaire{
     /* Surdefinition fonction partenaire */
     @Override
     public String get_categorie_avis() {
-        // TODO Auto-generated method stub
-        return null;
+        return "CAV3";
     }
 
     @Override
     public String get_categorie_evaluation() {
-        // TODO Auto-generated method stub
-        return null;
+        return "CEV3";
     }
 
     @Override
     public String get_categorie_reservation() {
-        // TODO Auto-generated method stub
-        return null;
+        return "CRS3";
     }
 
     @Override
-    public double get_evaluation() throws Exception {
-        // TODO Auto-generated method stub
-        return 0;
+    public double get_evaluation()throws Exception{
+        double resultat = 0;
+        Connection c = null;
+        PreparedStatement prstm = null;
+        ResultSet rs = null;
+        try{
+            c = Database.get_connection();
+            prstm = c.prepareStatement("SELECT * FROM v_evaluation_transport WHERE id_transport = ?");
+            prstm.setString(1, this.get_id());
+            rs = prstm.executeQuery();
+            if(rs.next()){
+                resultat = rs.getDouble(2);
+            }
+        }catch(Exception e){
+            throw e;
+        }finally{
+            if(rs != null){ rs.close(); }
+            if(prstm != null){ prstm.close(); }
+            if(c != null){ c.close(); }
+        }
+        return resultat;
     }
+
     
 }
